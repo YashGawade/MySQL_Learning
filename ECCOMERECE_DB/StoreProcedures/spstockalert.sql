c1a546aa80048b7c0134219baf0c15c1d43d8a49
Yash Gawade <yashgavde9@gmail.com>
2025-07-25 14:27:14 +0530

    Add files via upload

---
+++ ECCOMERECE_DB/StoreProcedures/spstockalert.sql
@@ -0,0 +1,16 @@
+USE tflecommerce;
+DELIMITER //
+
+CREATE PROCEDURE LowStockAlert(
+    IN p_threshold INT
+)
+BEGIN
+    SELECT id, name, stock
+    FROM products
+    WHERE stock < p_threshold;
+END //
+
+DELIMITER ;
+
+
+CALL LowStockAlert(10);
